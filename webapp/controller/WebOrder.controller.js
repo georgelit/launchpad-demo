ee9bb4104bd6d9dd18a369af9e55127de9af2457
chungseunghwan <chungseunghwan@gmail.com>
2017-11-27 16:26:31 +0900

    WebOrder edit update

--- webapp/controller/WebOrder.controller.js
+++ webapp/controller/WebOrder.controller.js
@@ -8,6 +8,44 @@ sap.ui.define([
 				var oViewModel = new sap.ui.model.json.JSONModel();
 				oViewModel.loadData('./model/orderData.json');
 				this.getView().setModel(oViewModel, "orderModel");
+				
+				
+			// 	var oTemplate = new sap.m.ColumnListItem({
+			// 		cells: [
+			// 			new sap.m.Input({
+			// 				value: "{orderModel>seq}"
+			// 			}), new sap.m.Input({
+			// 				value: "{orderModel>req_date}"
+			// 			}), new sap.m.Input({
+			// 				value: "{orderModel>rdd_date}"
+			// 			}), new sap.m.Input({
+			// 				value: "{orderModel>kunag}"
+			// 			}), new sap.m.Input({
+			// 				value: "{orderModel>kunagt}"
+			// 			}), new sap.m.Input({
+			// 				value: "{orderModel>kunnr}"
+			// 			}), new sap.m.Input({
+			// 				value: "{orderModel>kunnrt}"
+			// 			}), new sap.m.Input({
+			// 				value: "{orderModel>matnr}"
+			// 			}), new sap.m.Input({
+			// 				value: "{orderModel>ord_lfimg}"
+			// 			}), new sap.m.Input({
+			// 				value: "{orderModel>req_qty}",
+			// 				description: "{orderModel>meins}"
+			// 			}), new sap.m.Input({
+			// 				value: "{orderModel>arv_date}"
+			// 			})
+			// 		]
+			// 	});
+				
+			// 	var oTable = this.getView().byId("idProductsTable");
+			// 	oTable.bindItems({
+			// 	// path: "/ProductCollection",
+			// 	template: oTemplate
+			// 	// key: "ProductId"
+			// }).setKeyboardMode(true);
+			
 		},
 		onBtn1Press: function() {
 			this.getView().byId("btn2").setPressed(false);
@@ -20,6 +58,13 @@ sap.ui.define([
 		onBtn3Press: function() {
 			this.getView().byId("btn1").setPressed(false);
 			this.getView().byId("btn2").setPressed(false);
+		},
+		rebindTable: function(oTemplate, sKeyboardMode) {
+			this.oTable.bindItems({
+				path: "/ProductCollection",
+				template: oTemplate,
+				key: "ProductId"
+			}).setKeyboardMode(sKeyboardMode);
 		}
 
 	});
